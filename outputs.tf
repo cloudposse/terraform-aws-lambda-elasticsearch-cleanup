70894ef7b71efdce02161cd424f747d7782c87e0
ikochnev <ikochnev@iponweb.net>
2020-04-07 11:30:03 +0300

    terraform 0.12 and dep modules fixes

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "security_group_id" {
-  value       = "${join(",", aws_security_group.default.*.id)}"
+  value       = join(",", aws_security_group.default.*.id)
   description = "Security Group ID of the Lambda "
 }
--- variables.tf
+++ variables.tf
@@ -1,43 +1,43 @@
 variable "enabled" {
-  type        = "string"
+  type        = string
   default     = "true"
   description = "This module will not create any resources unless enabled is set to \"true\""
 }
 
 variable "es_endpoint" {
-  type        = "string"
+  type        = string
   description = "The Elasticsearch endpoint for the Lambda function to connect to"
 }
 
 variable "es_domain_arn" {
-  type        = "string"
+  type        = string
   description = "The Elasticsearch domain ARN"
 }
 
 variable "es_security_group_id" {
-  type        = "string"
+  type        = string
   description = "The Elasticsearch cluster security group ID"
 }
 
 variable "schedule" {
-  type        = "string"
+  type        = string
   default     = "cron(0 3 * * ? *)"
   description = "CloudWatch Events rule schedule using cron or rate expression"
 }
 
 variable "subnet_ids" {
-  type        = "list"
+  type        = list
   description = "Subnet ids"
 }
 
 variable "sns_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "SNS ARN to publish alerts"
 }
 
 variable "index" {
-  type        = "string"
+  type        = string
   default     = "all"
   description = "Index/indices to process. Use a comma-separated list. Specify `all` to match every index except for `.kibana` or `.kibana_1`"
 }
@@ -48,35 +48,35 @@ variable "delete_after" {
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   default     = "app"
   description = "Solution name, e.g. 'app' or 'cluster'"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map
   default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
 }
@@ -87,7 +87,7 @@ variable "index_format" {
 }
 
 variable "python_version" {
-  type        = "string"
+  type        = string
   default     = "2.7"
   description = "The Python version to use"
 }
@@ -98,6 +98,6 @@ variable "timeout" {
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "The VPC ID for the Lambda function"
 }
