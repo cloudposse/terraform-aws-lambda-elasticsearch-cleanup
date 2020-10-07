17240b619dc51ef0b5dfb8a1a56df943ecdc629d
Nuru <Nuru@users.noreply.github.com>
2020-10-07 11:52:08 -0700

    Update expected test results to reflect smaller ES domain

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -33,12 +33,14 @@ func TestExamplesComplete(t *testing.T) {
 	// Run `terraform output` to get the value of an output variable
 	privateSubnetCidrs := terraform.OutputList(t, terraformOptions, "private_subnet_cidrs")
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, []string{"172.16.0.0/19", "172.16.32.0/19"}, privateSubnetCidrs)
+	// assert.Equal(t, []string{"172.16.0.0/19", "172.16.32.0/19"}, privateSubnetCidrs)
+	assert.Equal(t, []string{"172.16.0.0/19"}, privateSubnetCidrs)
 
 	// Run `terraform output` to get the value of an output variable
 	publicSubnetCidrs := terraform.OutputList(t, terraformOptions, "public_subnet_cidrs")
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, []string{"172.16.96.0/19", "172.16.128.0/19"}, publicSubnetCidrs)
+	// assert.Equal(t, []string{"172.16.96.0/19", "172.16.128.0/19"}, publicSubnetCidrs)
+	assert.Equal(t, []string{"172.16.96.0/19"}, publicSubnetCidrs)
 
 	// Run `terraform output` to get the value of an output variable
 	domainHostname := terraform.Output(t, terraformOptions, "domain_hostname")
@@ -59,5 +61,5 @@ func TestExamplesComplete(t *testing.T) {
 	// Run `terraform output` to get the value of an output variable
 	lambdaFunctionArn := terraform.Output(t, terraformOptions, "lambda_function_arn")
 	// Verify we're getting back the outputs we expect
-	assert.Contains(t, lambdaFunctionArn, "function:eg-test-app-elasticsearch-cleanup")
+	assert.Contains(t, lambdaFunctionArn, "function:eg-test-elasticsearch-cleanup")
 }
